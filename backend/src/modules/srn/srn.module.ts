6bd4195e6d6012b160f0b1e144c6df596e5e462b
harpreet kaur gothra <harpreetk2146@gmail.com>
2026-01-29 04:50:24 +0530

    Refactor SRN module wiring for assignment-aware request handling

--- backend/src/modules/srn/srn.module.ts
+++ backend/src/modules/srn/srn.module.ts
@@ -4,9 +4,11 @@ import { SRNService } from './srn.service';
 import { PrismaService } from '../../common/prisma.service';
 import { SequenceService } from '../../common/sequence.service';
 import { ManufacturerInventoryModule } from '../manufacturer-inventory/manufacturer-inventory.module';
+import { AssignmentModule } from '../assignment/assignment.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [ManufacturerInventoryModule],
+  imports: [ManufacturerInventoryModule, AssignmentModule, NotificationModule],
   controllers: [SRNController],
   providers: [SRNService, PrismaService, SequenceService],
   exports: [SRNService],
